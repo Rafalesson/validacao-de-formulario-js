987fc40c8cf092a4483716d36fafaae0fc8c9142
Rafalesson <rafaalesson35@gmail.com>
2022-11-17 23:56:08 -0300

    validação dos campos: nome, email, senha e data de nascimento

---
+++ assets/js/validacao.js
@@ -0,0 +1,73 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+];
+
+const mensagemDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, e deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior de 18 anos para se cadastrar.'
+    },
+
+}
+
+const validadores = {
+    dataNascimento: input => validaNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = '';
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagemDeErro[tipoDeInput][erro];
+        }
+    });
+    return mensagem;
+}
+
+function validaNascimento(input) {
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
+
+    if (!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior de 18 anos para se cadastrar.';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function maiorQue18(data) { 
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+}
